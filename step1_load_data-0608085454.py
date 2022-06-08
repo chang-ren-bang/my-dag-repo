d80ec9b532aa603a847658aa867cf0f7671529a5
iambenben <12691048+chang-ren-bang@users.noreply.github.com>
2022-06-08 16:54:54 +0800

    Pushed DAG step1_load_data-0608085454

---
+++ step1_load_data-0608085454.py
@@ -0,0 +1,30 @@
+from airflow.providers.papermill.operators.papermill import PapermillOperator
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+
+args = {
+    "project_id": "step1_load_data-0608085454",
+}
+
+dag = DAG(
+    "step1_load_data-0608085454",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="""
+Created with Elyra 3.8.1 pipeline editor using `step1_load_data.pipeline`.
+    """,
+    is_paused_upon_creation=False,
+)
+
+
+# Operator source: {"catalog_type": "airflow-provider-package-catalog", "component_ref": {"provider_package": "apache_airflow_providers_papermill-2.2.3-py3-none-any.whl", "provider": "apache_airflow_providers_papermill", "file": "airflow/providers/papermill/operators/papermill.py"}}
+op_ac8738d1_42e1_4a83_b413_dcf8991cd77b = PapermillOperator(
+    task_id="load_data",
+    input_nb="~/work/examples/pipelines/run-generic-pipelines-on-apache-airflow/load_data.ipynb",
+    output_nb="~/work/examples/pipelines/run-generic-pipelines-on-apache-airflow/load_data_out.ipynb",
+    parameters={},
+    kernel_name="",
+    dag=dag,
+)
